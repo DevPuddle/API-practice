c80781689e55a7f4c2dff607b5d790a1fad68a0e
Isaiah Henson <Devpuddles@gmail.com>
2022-04-11 22:17:15 -0500

    first attempts at building an API

---
+++ app.js
@@ -0,0 +1,16 @@
+document.querySelector('button').addEventListener('click', generate)
+
+function generate() {
+    let sprites = document.querySelector('input').value
+    
+fetch(`https://pokeapi.co/api/v2/pokemon/${sprites}`)
+.then(res => res.json()) // parse response as JSON
+.then(data => {
+  console.log(data)
+  document.querySelector('img').src = data.sprites
+})
+.catch(err => {
+    console.log(`error ${err}`)
+});
+}
+
