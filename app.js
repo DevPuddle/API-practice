3557f2b5fbe263b16e24a0e2d6179e27def384b4
Isaiah Henson <Devpuddles@gmail.com>
2022-05-25 01:56:31 -0400

    api practice

--- app.js
+++ app.js
@@ -3,7 +3,10 @@ document.querySelector('button').addEventListener('click', generate)
 function generate() {
     let sprites = document.querySelector('input').value
     
-fetch(`https://pokeapi.co/api/v2/pokemon/${sprites}`)
+
+}
+
+fetch("https://pokeapi.co/api/v2/pokemon/ditto")
 .then(res => res.json()) // parse response as JSON
 .then(data => {
   console.log(data)
@@ -12,5 +15,3 @@ fetch(`https://pokeapi.co/api/v2/pokemon/${sprites}`)
 .catch(err => {
     console.log(`error ${err}`)
 });
-}
-
